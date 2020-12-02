7cafb10b901e22ba12aaa75a6c77f56ecebe3844
liuwell <liuwei_hnnd@163.com>
2020-12-02 23:27:28 +0800

    Updated for sperm.py

--- srat/sperm.py
+++ srat/sperm.py
@@ -35,43 +35,33 @@ def sperm(prefix, library, threads, collapser, devnull):
 	bowtie_mir_out = prefix + ".bowtie_miRNA.out"
 	subprocess.call("bowtie -v 0 %s/index_miRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_spikein, unmapped_mir, bowtie_mir_out), shell=True, stderr=devnull)
 	
-	# for sperm piRNA
-	unmapped_piRNA = prefix + ".unmap_piRNA.fa"
-	bowtie_piRNA_out = prefix + ".bowtie_piRNA.out"
-	subprocess.call("bowtie -v 0 %s/index_piRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_mir, unmapped_piRNA, bowtie_piRNA_out), shell=True, stderr=devnull)
-	
 	# for tsRNA
 	unmapped_tsRNA = prefix + ".unmap_tsRNA.fa"
 	bowtie_tsRNA_out = prefix + ".bowtie_tsRNA.out"
-	subprocess.call("bowtie -v 0 %s/index_tsRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_piRNA, unmapped_tsRNA, bowtie_tsRNA_out), shell=True, stderr=devnull)
+	subprocess.call("bowtie -v 0 %s/index_tsRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_mir, unmapped_tsRNA, bowtie_tsRNA_out), shell=True, stderr=devnull)
 
 	# for rsRNA
 	unmapped_rsRNA = prefix + ".unmap_rsRNA.fa"
 	bowtie_rsRNA_out = prefix + ".bowtie_rsRNA.out"
 	subprocess.call("bowtie -v 0 %s/index_rsRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_tsRNA, unmapped_rsRNA, bowtie_rsRNA_out), shell=True, stderr=devnull)
 
-
-	# for other RNA
-	unmapped_RNA = prefix + ".unmap_RNA.fa"
-	bowtie_RNA_out = prefix + ".bowtie_RNA.out"
-	subprocess.call("bowtie -v 0 %s/index_RNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_rsRNA, unmapped_RNA, bowtie_RNA_out), shell=True, stderr=devnull)
+	# for piRNA
+	unmapped_piRNA = prefix + ".unmap_piRNA.fa"
+	bowtie_piRNA_out = prefix + ".bowtie_piRNA.out"
+	subprocess.call("bowtie -v 0 %s/index_piRNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_rsRNA, unmapped_piRNA, bowtie_piRNA_out), shell=True, stderr=devnull)
 	
-	'''
-	# for tsRNA, rsRNA and other RNA
+	# for other RNA
 	unmapped_RNA = prefix + ".unmap_RNA.fa"
 	bowtie_RNA_out = prefix + ".bowtie_RNA.out"
 	subprocess.call("bowtie -v 0 %s/index_RNA --norc --suppress 6,7 -p %d -f %s --un %s > %s" % (library, threads, unmapped_piRNA, unmapped_RNA, bowtie_RNA_out), shell=True, stderr=devnull)
 	
-	'''
 	# for genome
 	map_genome = prefix + ".map_genome.fa"
 	unmap_genome = prefix + ".unmap_genome.fa"
 	subprocess.call("bowtie -v 0 %s/genome -p %d -f %s --al %s --un %s " % (library, threads, unmapped_RNA, map_genome, unmap_genome), shell=True, stderr=devnull, stdout=devnull)
 	# combined
 	bowtie_out_combined = prefix + ".bowtie_combined.out"
-	#os.system("cat %s %s %s> %s" % (bowtie_mir_out, bowtie_piRNA_out, bowtie_RNA_out, bowtie_out_combined))
-	#os.system("rm %s %s %s %s" % (unmapped_spikein, unmapped_mir, unmapped_piRNA, unmapped_RNA))
-	os.system("cat %s %s %s %s %s> %s" % (bowtie_mir_out, bowtie_piRNA_out, bowtie_tsRNA_out, bowtie_rsRNA_out,  bowtie_RNA_out, bowtie_out_combined))
+	os.system("cat %s %s %s %s %s > %s" % (bowtie_mir_out, bowtie_piRNA_out, bowtie_tsRNA_out, bowtie_rsRNA_out,  bowtie_RNA_out, bowtie_out_combined))
 	os.system("rm %s %s %s %s %s %s" % (unmapped_spikein, unmapped_mir, unmapped_piRNA, unmapped_rsRNA, unmapped_tsRNA, unmapped_RNA))
 
 	return bowtie_spikein_out, bowtie_out_combined, map_genome, unmap_genome
@@ -79,7 +69,7 @@ def sperm(prefix, library, threads, collapser, devnull):
 ###
 def sperm_RNA(bowtie_out_combined, prefix):
 	'''
-	get tsRNA, rsNA piRNA, profile
+	get tsRNA, rsRNA, piRNA and profile
 	'''
 	dic_miR = defaultdict(int)
 	dic_miR_iso = defaultdict(int)
@@ -97,23 +87,22 @@ def sperm_RNA(bowtie_out_combined, prefix):
 
 			### miRNA
 			if re.search("miRNA", seg[2]):
-				mir = seg[2].split("|")[0]
-				dic_miR[mir]+=count
+				pass
+				#mir = seg[2].split("|")[0]
+				#dic_miR[mir]+=count
 				### no 5p isoform
-				if seg[3] == "0":
-					dic_miR_iso[mir]+=count
-
+				#if seg[3] == "0":
+				#	dic_miR_iso[mir]+=count
+			### tsRNA
 			elif re.search("tsRNA", seg[2]):
 				tsR = seg[4] + "|" + seg[2]
 				dic_tsRNA[tsR] += count
-			
+			### rsRNA
 			elif re.search("rsRNA", seg[2]):
 				rsR = seg[4] + "|" + seg[2]
 				dic_rsRNA[rsR] += count
 			### piRNA
 			elif re.search("piRNA", seg[2]):
-				#piRNA_cluster.add(pir_c)
-				#piRNA_seq.add(seg[4])
 				dic_piR[seg[4]]+=count
 				pir_c = seg[2].split("|")[0]
 				dic_piR_cluster[pir_c]+=count
@@ -130,87 +119,12 @@ def sperm_RNA(bowtie_out_combined, prefix):
 	rsRNA_series.to_csv(rsRNA_out, header=False, sep='\t')
 	
 	### piRNA
-	#dic_piR = sorted(dic_piR.items(), key=lambda d:d[1], reverse=True)
 	piRNA_out = prefix + ".piRNA_seq.txt"
 	piRNA_series = Series(dic_piR)
 	piRNA_series.to_csv(piRNA_out, header=False, sep='\t')
-	#dic_piR_cluster = sorted(dic_piR_cluster.items(), key=lambda d:d[1], reverse=True)
 	piRNA_out2 = prefix + ".piRNA_cluster.txt"
 	piRNA_series2 = Series(dic_piR_cluster)
 	piRNA_series2.to_csv(piRNA_out2, header=False, sep='\t')
-
-###
-'''
-def spermProcess(bowtie_out_combined, prefix, cut_adapt, collapser, map_genome, bowtie_spikein_out):
-
-	dic_miR = defaultdict(int)         ### count miRNA 
-	dic_miR_5p = defaultdict(int)      ### count miRNA, 5p in align
-	dic_type = defaultdict(int)        ### count RNA type
-	
-	total_RNA = 0
-	dic_length_RNA = {}  ### length distribution of all different kinds mapped RNAs
-
-	with open(bowtie_out_combined) as handle:
-		for line in handle:
-			seg =line.split()
-			count = int(seg[0].split("-")[1])
-
-			### miRNA
-			if re.search("miRNA", seg[2]):
-				mir = seg[2].split("|")[0]
-				dic_miR[mir] += count
-			
-			title_split = seg[2].split("|")
-			RNA_type = title_split[-1]
-			dic_type[RNA_type] += count
-			total_RNA += count
-
-			k_map = len(seg[4])
-			k_RNA = RNA_type
-			if k_map in dic_length_RNA:
-				if k_RNA in dic_length_RNA[k_map]:
-					dic_length_RNA[k_map][k_RNA] += count
-
-				else:
-					dic_length_RNA[k_map][k_RNA] = count
-
-			else:
-				tmp_dic = {}
-				tmp_dic[k_RNA] = count
-				dic_length_RNA[k_map] = tmp_dic
-
-	### summary RNA counts
-	dic_type["totals"] = get_count(cut_adapt)
-	dic_type["useful"] = get_count(collapser)
-	useful_ratio = round(dic_type["useful"]/dic_type["totals"]*100, 2)
-	dic_type["useful_ratio(%)"] = useful_ratio
-	###
-	dic_type["total_map"] = get_count(map_genome)+total_RNA
-	map_ratio = round(dic_type["total_map"]/dic_type["useful"]*100, 2)
-	dic_type["map_ratio(%)"] = map_ratio
-	###
-	dic_type["total_knownRNA"] = total_RNA
-	RNA_ratio = round(dic_type["total_knownRNA"]/dic_type["total_map"]*100, 2)
-	dic_type["knownRNA_ratio(%)"] = RNA_ratio
-	###
-	dic_type["miRNA_kinds"] = len(dic_miR)
-	### for spikein
-	with open(bowtie_spikein_out) as handle:
-		sum_spikein = sum([int(i.split()[0].split("-")[1]) for i in handle])
-	dic_type["spikein"] = sum_spikein
-	spikein_ratio = round(dic_type["spikein"]/dic_type["total_map"]*100, 2)
-	dic_type["spikein_ratio(map%)"] = spikein_ratio
-
-	###
-	dic_type = OrderedDict(sorted(dic_type.items(), key=lambda d:d[1], reverse=True))
-	summary_out = prefix+'.summary_count.txt'
-	summary_series = Series(dic_type)
-	summary_series.to_csv(summary_out, header=False, sep='\t')
-
-	dic_length_RNA = DataFrame(dic_length_RNA).T
-
-	return dic_length_RNA, total_RNA, dic_miR, dic_miR_5p, dic_type, sum_spikein
-'''
 
 ###################
 def spermPlot(dic_length_RNA, total_RNA, prefix):
@@ -219,7 +133,6 @@ def spermPlot(dic_length_RNA, total_RNA, prefix):
 	# sort by index, ascending
 	df = df.sort_index(axis=0, ascending=True)
 	
-	#df_RNA = df.loc[:,['miRNA', 'tsRNA', 'rsRNA', 'snoRNA', 'lncRNA', 'mRNA']]
 	df_RNA = df.loc[:,['miRNA', 'tsRNA', 'rsRNA', 'piRNA', 'snoRNA', 'lncRNA', 'mRNA']]
 
 	df_RNA_other = pd.DataFrame(df.sum(axis=1) - df_RNA.sum(axis=1),columns=["others"])
@@ -250,7 +163,6 @@ def spermPlot(dic_length_RNA, total_RNA, prefix):
 	pie_RNA = prefix + ".pie_RNA.pdf"
 	df_RNA_sum = df_RNA.sum(axis=0)
 	df_RNA_sum.name = ''
-	#df_RNA_sum.name = dir_name
 	df_RNA_sum.plot(kind='pie', figsize=(6,6), colors=colors, autopct='%.1f',fontsize=15 )
 	pie_csv = prefix + ".pie_RNA.txt"
 	df_RNA_sum = df_RNA_sum.fillna(value=0)
